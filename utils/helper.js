401e50163c43cffb77c212a4257f20b4f1bf12a6
Hailey Bates <hailey.bates001@gmail.com>
2022-06-04 00:43:13 -0400

    Saving

--- utils/helper.js
+++ utils/helper.js
@@ -1,1 +1,6 @@
-// Create the time stamps for the posts/comments here
+module.exports = {
+    format_date: (date) => {
+      // Format date as MM/DD/YYYY
+      return date.toLocaleDateString();
+    }
+}
