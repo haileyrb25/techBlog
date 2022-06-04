c0ee4f4f21037e017b54790fb93d7069b38746f4
Hailey Bates <hailey.bates001@gmail.com>
2022-06-03 23:56:25 -0400

    Finished models: please note, a lot of code was helper code and the rest was taken from prior mini project activity

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,40 @@
-// Finish the rest of this model
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    comment_title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Post'
+  }
+);
+
+module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -2,17 +2,23 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-Post.belongsTo(User, {
-    // fill out the rest of the belongsTo relationship
+Comment.belongTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
+
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
 
 Post.hasMany(Comment, {
-    // fill out the rest of the hasMany relationship
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
 });
 
-Comment.belongTo(User, {
-    // fill out the rest of the belongsTo relationship
-});
 
 module.exports = {
     User,
