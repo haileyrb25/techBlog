bbce28b81c031640c4ec9b74fffab15ef5185857
Hailey Bates <hailey.bates001@gmail.com>
2022-06-03 23:49:56 -0400

    Finished user model

--- models/User.js
+++ models/User.js
@@ -3,12 +3,13 @@ const bcrypt = require("bcrypt");
 const sequelize = require("../config/config");
 
 class User extends Model {
-    // set up a method to run on instance data (per user) to check passwords
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
 };
 
 User.init(
     { 
-        // Finish the user model
         id: {
             type: DataType.INTEGER,
             allowNull: false,
@@ -16,18 +17,31 @@ User.init(
             autoIncrement: true
         },
         username: {
-
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+            len: [8],
+            },
         },
         password: {
-
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+            len: [8],
+            },
         },
     },
     {
-    hooks: {
-        // set up beforeCreate lifecycle "hook"
-        // beforeCreate: async () => {},
-        // beforeUpdate: async () => {}
-    },
+        hooks: {
+            beforeCreate: async (newUserData) => {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+              return updatedUserData;
+            },
+          },
     sequelize,
     timestamps: false,
     freezeTableName: true,
